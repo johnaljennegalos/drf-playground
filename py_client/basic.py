a63bf53b227474546e6cd4812bb8404dcfd3dce5
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-06-05 22:05:19 +0800

    feat: basic api
    fetch basic api data from endpoint

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,6 +2,8 @@ import requests
 
 endpoint = "https://httpbin.org/anything"
 
-get_response = requests.get(endpoint)
+get_response = requests.get(endpoint, json={"Query" : "Hello John"})
 
 print(get_response.text)
+
+print(get_response.json())
