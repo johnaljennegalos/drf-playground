321c7ee0b8710744cfd277e0ec71fe16454b9b1b
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-06-05 22:44:50 +0800

    feat: api connection
    successfully setup api path and connect them

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,12 +1,12 @@
 import requests
 
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://localhost:8000/"
+endpoint = "http://localhost:8000/api/"
 
 get_response = requests.get(endpoint, json={"Query" : "Hello John"})
 
 print(get_response.text)
 print(get_response.status_code)
 
-# print(get_response.json())
+print(get_response.json())
 # print(get_response.status_code)
