071b2f1e3f91f3ae2e164e8020531908546f2f98
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-06-05 22:20:33 +0800

    test: django api
    test django api connection

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,9 +1,12 @@
 import requests
 
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/"
 
 get_response = requests.get(endpoint, json={"Query" : "Hello John"})
 
 print(get_response.text)
+print(get_response.status_code)
 
-print(get_response.json())
+# print(get_response.json())
+# print(get_response.status_code)
