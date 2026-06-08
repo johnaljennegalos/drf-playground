d086295769defd8ad80e00dc9d66fc14a8c692b1
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-06-08 18:14:14 +0800

    test: API view conversion
    convert the home view into a rest api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,35 +1,17 @@
 import json
 from django.forms.models import model_to_dict
-from django.http import JsonResponse
+# from django.http import JsonResponse, HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products.models import Products
 
+@api_view
 def api_home(request, *args, **kwargs):
-
     model_data = Products.objects.all().order_by("?").first()
     data = {}
 
     if model_data:
         data = model_to_dict(model_data)
         #data = model_t0_dict(model_data, fields=['id', 'title']
-
-        #data['id'] = model_data.id
-        #data['title'] = model_data.title
-        #data['content'] = model_data.content
-        #data['price'] = model_data.price
-
-    # print(request.GET)
-    # body = request.body
-    # data = {}
-    # try:
-    #     data = json.loads(body)
-    # except:
-    #     pass
-    #
-    # print(data)
-    #
-    # data['params'] = dict(request.GET)
-    # data["headers"] = dict(request.headers)
-    # data['content_type'] = request.content_type
-
-    return JsonResponse(data)
+    return Response(data)
