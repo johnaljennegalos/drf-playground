6a141a53bf7d8f3cfc1cbdbcc2994c0a559ab402
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-06-08 19:41:53 +0800

    feat: product serializer
    Implement a product serializers to return serialize data

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,15 +3,17 @@ from django.forms.models import model_to_dict
 # from django.http import JsonResponse, HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
 from products.models import Products
+from products.serializers import ProductsSerializer
+
 
 @api_view(["GET"])
 def api_home(request, *args, **kwargs):
-    model_data = Products.objects.all().order_by("?").first()
+    instance = Products.objects.all().order_by("?").first()
     data = {}
 
-    if model_data:
+    if instance:
         #data = model_to_dict(model_data)
-        data = model_to_dict(model_data, fields=['id', 'title', 'price', 'sale_price'])
+        #data = model_to_dict(model_data, fields=['id', 'title', 'price', 'sale_price'])
+        data = ProductsSerializer(instance).data
     return Response(data)
---
+++ backend/products/serializers.py
@@ -0,0 +1,20 @@
+from dataclasses import fields
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+
+from .models import Products
+
+class ProductsSerializer(serializers.ModelSerializer):
+    my_discount = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = Products
+        fields = [
+            'title',
+            'content',
+            'price',
+            'sale_price',
+            'my_discount',
+        ]
+
+    def get_my_discount(self, obj):
+        return obj.get_discount()
