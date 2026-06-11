60351733ef506dab3950ad9b22b760e23f921056
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-06-11 22:13:26 +0800

    test: create api view
    test the CreateAPIView to check if a post request will be received, validated and stored in the DB.

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,6 +1,21 @@
+from certifi import contents
 from rest_framework import generics
 from .models import Products
 from .serializers import ProductsSerializer
+
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
+
+    def perform_create(self, serializer):
+        print(serializer.validated_data)
+
+        title = serializer.validated_data['title']
+        content = serializer.validated_data['content'] or None
+
+        if content is None:
+            content = title
+        serializer.save(content=content)
 
 class ProductDetailsAPIView(generics.RetrieveAPIView):
     queryset = Products.objects.all()
