b391e703659908abfcd0309e0649b45604242ea3
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-06-13 22:28:18 +0800

    test: function based views
    test a function based views to intercept incoming method request if get or post and use if/elif method to create the 3 generic list views

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,5 +1,9 @@
 from certifi import contents
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
 from .models import Products
 from .serializers import ProductsSerializer
 
@@ -38,3 +42,32 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
         if content is None:
             content = title
         serializer.save(content=content)
+
+@api_view(['GET', 'POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+
+    if method == 'GET':
+        #detail view
+        if pk is not None:
+            obj = get_object_or_404(Products, pk=pk)
+            data  = ProductsSerializer(obj, many=False).data
+            return Response(data)
+
+
+        #list view
+        queryset = Products.objects.all()
+        data = ProductsSerializer(queryset, many=True).data
+        return Response(data)
+
+    if method == 'POST':
+        serializer = ProductsSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data['title']
+            content = serializer.validated_data['content'] or None
+
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response({"message": "Invalid data"}, status=400)
